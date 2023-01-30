bb94b8aacd8f170ee33e9e9865008ba12bad1b7c
fjmiguel0704 <fernandoj.miguel@iesnervion.es>
2023-01-30 09:11:53 +0100

    Parte Javadoc terminada

--- ExEntornos-Tema3-bis/src/fecha/Fecha.java
+++ ExEntornos-Tema3-bis/src/fecha/Fecha.java
@@ -1,5 +1,12 @@
 package fecha;
 
+/**
+ * Clase en la que se comprobará si la fecha es correcta o no, teniendo en
+ * cuenta los meses del año, días que tiene, etc.
+ * 
+ * @author fjmiguel
+ *
+ */
 public class Fecha {
 	private int dia;
 	private int mes;
@@ -7,11 +14,22 @@ public class Fecha {
 
 	// Constructores
 	// Constructor por defecto
+
+	/**
+	 * Método por defecto
+	 */
 	public Fecha() {
 
 	}
 
 	// Constructor con parámetros
+	/**
+	 * Este método guardará información del día, mes y año
+	 * 
+	 * @param dia  contiene el día de la fecha
+	 * @param mes  contiene en mes de la fecha
+	 * @param anio contiene el año de la fecha
+	 */
 	public Fecha(int dia, int mes, int anio) {
 		this.dia = dia;
 		this.mes = mes;
@@ -19,6 +37,11 @@ public class Fecha {
 	}
 
 	// Constructor copia
+	/**
+	 * Este método crea una copia del anterior, con el dia, mes y año
+	 * 
+	 * @param f hará referencia al día, mes y año
+	 */
 	public Fecha(final Fecha f) {
 		dia = f.dia;
 		mes = f.mes;
@@ -26,31 +49,66 @@ public class Fecha {
 	}
 
 	// getters y setters
+	/**
+	 * Esta función obtiene el valor de la variable dia y lo devuelve
+	 * 
+	 * @return el dia de la fecha
+	 */
 	public int getDia() {
 		return dia;
 	}
 
+	/**
+	 * Este método modifica el valor de la variable dia
+	 * 
+	 * @param dia dia de la fecha introducido por parámetro
+	 */
 	public void setDia(int dia) {
 		this.dia = dia;
 	}
 
+	/**
+	 * Esta función obtiene el valor de la variable mes y lo devuelve
+	 * 
+	 * @return el mes de la fecha
+	 */
 	public int getMes() {
 		return mes;
 	}
 
+	/**
+	 * Este método modifica el valor de la variable mes
+	 * 
+	 * @param mes mes de la fecha introducido por parámetro
+	 */
 	public void setMes(int mes) {
 		this.mes = mes;
 	}
 
+	/**
+	 * Este método obtiene el valor de la variable anio y lo devuelve
+	 * 
+	 * @return el año de la fecha
+	 */
 	public int getAnio() {
 		return anio;
 	}
 
+	/**
+	 * Este método modifica el valor de la variable anio
+	 * 
+	 * @param anio año de la fecha introducido por parámetro
+	 */
 	public void setAño(int anio) {
 		this.anio = anio;
 	}
 
 	// Método fecha correcta
+	/**
+	 * Este método comprobará caso por caso si la fecha es correcta o no lo es
+	 * 
+	 * @return la fecha (dia, mes, año) correcta
+	 */
 	public boolean fechaCorrecta() {
 		boolean diaCorrecto, mesCorrecto, anioCorrecto;
 		anioCorrecto = anio > 0;
@@ -76,12 +134,22 @@ public class Fecha {
 	}
 
 	// Método esBisiesto. Solo lo usa fechaCorrecta, por eso es privado
+	/**
+	 * Este método comprobará si el año de la fecha correcta comprobada
+	 * anteriormente es Bisiesto o no
+	 * 
+	 * @return true en caso de ser Bisiesto
+	 */
 	private boolean esBisiesto() {
 		boolean esBisiesto = (anio % 4 == 0 && anio % 100 != 0 || anio % 400 == 0);
 		return esBisiesto;
 	}
 
 	// Método diaSiguiente
+	/**
+	 * Esta función incrementará el mes del año cuando el dia sea igual a 1 e
+	 * incrementará el año cuando el mes sea igual a 1
+	 */
 	public void diaSiguiente() {
 		dia++;
 		if (!fechaCorrecta()) {
@@ -95,6 +163,12 @@ public class Fecha {
 	}
 
 	// Método toString
+	/**
+	 * Este método irá comprobando si el dia y mes es menor, mayor o igual a 10 para
+	 * mostrar el formato correcto de la fecha
+	 * 
+	 * @return la fecha final 
+	 */
 	public String toString() {
 		String fechaFinal = "";
 		if (dia < 10 && mes < 10) {
